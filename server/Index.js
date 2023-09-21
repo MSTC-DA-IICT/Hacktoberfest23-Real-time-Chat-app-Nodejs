d942d5e9d4e828cf2c0abe22f3d76b3de1c2b67b
Harsh Mungara <harsh.mungara2004@gmail.com>
2023-09-21 18:23:11 +0530

    server

---
+++ server/Index.js
@@ -0,0 +1,16 @@
+//Node server yo handle socket io connections
+
+const Io = require('socket.io')(8000)
+
+const users = {}
+
+Io.on('connection', socket =>{
+    socket.on('new-user-joined',name=>{
+        users[socket.id] =name;
+        socket.broadcast.emit('user-joined',name);
+    })
+
+    socket.on('send',message =>{
+        socket.broadcast.emit('receive', {message: message, name: user[socket.id]})
+    })
+})
