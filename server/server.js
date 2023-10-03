f7bee7517f9113007751bc0ad2e1e6a76592411c
Priyanshu Gagiya <priyanshuahir78@gmail.com>
2023-10-03 22:12:22 +0530

    user joined broadcast feature added

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
-//Node server yo handle socket io connections
+// Node server to handle socket.io connections
+const Io = require('socket.io')(8000);
 
-const Io = require('socket.io')(8000)
+const users = {};
 
-const users = {}
-
-Io.on('connection', socket =>{
-    socket.on('new-user-joined',name1=>{
-        console.log("New user",name1)
-        users[socket.id] =name1;
-        socket.broadcast.emit('user-joined',name1);
+Io.on('connection', socket => {
+    socket.on('new-user-joined', name1 => {
+        console.log("New user", name1);
+        users[socket.id] = name1;
+        // Broadcast the username and a message
+        socket.broadcast.emit('user-joined', { name: name1, message: 'has joined the chat' });
     });
 
-    socket.on('send',message =>{
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
     });
-})
+});
