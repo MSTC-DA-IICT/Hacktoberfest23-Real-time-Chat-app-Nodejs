63f6bf84580dd2ec9d982527d0244b35c17aff2a
Jay Sabva <202101224@daiict.ac.in>
2023-10-18 18:19:27 +0530

    Created Middleware and some modification

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/user');
+const checkAuth = require('../middleware/check-auth');
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
-router.get('/logout', userController.logoutUser);
-router.get('/', userController.getAllUsers);
+router.get('/', checkAuth, userController.getAllUsers);
 
 module.exports = router;
